55c03a6eabd8fe1a9d4e982f2bdc46d194af759d
Terry Kwon <terry.kwon@sendbird.com>
2017-03-30 19:35:31 +0900

    Remove unnecessary import

--- app/src/main/java/com/sendbird/android/sample/groupchannel/GroupChatAdapter.java
+++ app/src/main/java/com/sendbird/android/sample/groupchannel/GroupChatAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
